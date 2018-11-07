249839f9eb18e06b4305f9cb2b7dd21c6d7d70d5
Faracoeng <alfmazucheli@gmailcom>
2018-11-07 20:35:53 -0200

    inicio(algo feito)

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class app {
 
@@ -11,10 +13,48 @@ public class app {
     private JButton a7Button;
     private JButton a2Button;
     private JPanel painelPrincipal;
+    private JButton a9Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a1Button;
+    private JButton a3Button;
+    private JButton a0Button;
+    private JButton ponto;
+    private JButton equal;
+    private JButton a8Button;
+
+    public app() {
+        painelPrincipal.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        a8Button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        MouseAdapter listener = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        };
+        a7Button.addMouseListener(listener);
+        a9Button.addMouseListener(listener);
+        a4Button.addMouseListener(listener);
+        a5Button.addMouseListener(listener);
+        a6Button.addMouseListener(listener);
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
     }
+
+
 
 
     public static void main(String[] args) {
